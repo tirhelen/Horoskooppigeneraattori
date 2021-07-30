7b629750813b1dca3e21aaf8477082f8e66ac3b3
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2021-07-30 18:51:57 +0300

    test for filereader

---
+++ src/tests/file_reader_test.py
@@ -0,0 +1,11 @@
+import unittest
+import os
+from file_reader import read_file
+
+class TestFileReader(unittest.TestCase):
+    def test_correct_list_from_file(self):
+        current_dir = os.path.dirname(__file__)
+        parent_dir = os.path.split(current_dir)[0]
+        file = os.path.join(current_dir, "testfile.txt")
+        list = read_file(file)
+        self.assertEqual(list, ["Ihan", "tavallinen", ",", "tekstitiedosto", "vain", "."])
