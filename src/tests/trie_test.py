33c3bfad8e24b39c68dc5e0c9406401d2ea51ca4
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2021-08-05 12:02:58 +0300

    test file updated

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -1,25 +1,22 @@
+# -*- coding: utf-8 -*-
 import unittest
 from trie import Trie
 
 class TestTrie(unittest.TestCase):
     def setUp(self):
-        self.trie = Trie()
+        self.list = ["Tässä", "on", "todella", "tärkeää", "tekstiä", ".",
+        "tärkeää", "tekstiä", ",", "jota", "ei", "saa",
+        "hukata", ".", "ei", "milloinkaan", "."]
+        self.trie = Trie(self.list)
 
     def test_correct_key_values(self):
-        self.list = ["Tässä", "on", "todella", "tärkeää", "tekstiä", ".",
-                "tärkeää", "tekstiä", ",", "jota", "ei", "saa",
-                "hukata", ".", "ei", "milloinkaan", "!"]
         help_list = []
-        for i in range(len(self.list)):
-            if i == len(self.list)-1:
-                self.trie.add_edge(self.list[i], None)
-
-            else:
-                self.trie.add_edge(self.list[i], self.list[i+1])
-
+        for i in range(len(self.list)-1):
             if self.list[i] not in help_list:
                 help_list.append(self.list[i])
-
+            if self.list[i] + " " + self.list[i+1] not in help_list and self.list[i+1] != ".":
+                help_list.append(self.list[i] + " " + self.list[i+1])
         keys = sorted(self.trie.hashmap.keys())
         help_list = sorted(help_list)
         self.assertEqual(keys, help_list)
+    
