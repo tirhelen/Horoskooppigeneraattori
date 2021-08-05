c1b6cf81d94652d8f6d9f559080356b386d1ebda
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2021-08-05 12:03:50 +0300

    tasks updates

--- tasks.py
+++ tasks.py
@@ -7,3 +7,11 @@ def lint(ctx):
 @task
 def test(ctx):
 	ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+	ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+	ctx.run("coverage html")
