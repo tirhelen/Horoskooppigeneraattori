b1e7d3442628ab147fa03b581148612a73dcfa9f
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2021-08-13 13:26:19 +0300

    markov chain fixed with longer text and higher degree

--- src/markov_chain.py
+++ src/markov_chain.py
@@ -2,7 +2,7 @@
 import random
 
 
-def create_sentence(dict, start, sentence):
+def create_sentence(dict, start, sentence, degree, help_list, length):
     """luo virkkeen käyttäen toisen asteen Markovin ketjua
 
     Args:
@@ -13,16 +13,30 @@ def create_sentence(dict, start, sentence):
     Returns:
         sentence (list): lista lopullisesta lauseesta
     """
+    if sentence.count(". ") == length:
+        return sentence
+
     choices = []
     for x in dict[start]:
-        for i in range(x[1]):
+        for j in range(x[1]):
             if x[0] not in ["JOUSIMIES", "KAKSONEN", "RAPU", "LEIJONA",
                             "VESIMIES", "NEITSYT", "SKORPIONI", "HÄRKÄ",
                             "OINAS", "VAAKA", "KAURIS", "KALAT"]:
                 choices.append(x[0])
-    next = random.choice(choices)
+            else:
+                choices.append(".")
+
+    next = random.choice(choices) + " "
+
+    if next == ". " and start[-2] == ".":
+        return create_sentence(dict, ". ", sentence, degree, [], length)
+
     sentence.append(next)
-    if next == ".":
-        return sentence
-    next = sentence[-2] + " " + sentence[-1]
-    return create_sentence(dict, next, sentence)
+    help_list.append(next)
+
+    if len(help_list) == degree+1:
+        help_list.pop(0)
+
+    next = ''.join(map(str, help_list))
+
+    return create_sentence(dict, next, sentence, degree, help_list, length)
